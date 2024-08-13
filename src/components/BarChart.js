29813893768298a614bdee8283a548985c159cf3
scoredevil <abdullahelshmoty@gmail.com>
2024-08-13 17:00:51 +0300

    solved bug + reduced delay

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import Parse from 'parse/dist/parse.min.js';
 import env from "react-dotenv";
 import { BarChart } from '@mui/x-charts/BarChart';
-import './BarChart.css'
+import './BarChart.css';
+
 
 Parse.initialize(env.PARSE_APPLICATION_ID, env.PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = env.PARSE_HOST_URL;
@@ -12,20 +13,24 @@ export default function Bar() {
 
     React.useEffect(() => {
         const fetchDataAndMigrate = async () => {
-            const Polls = Parse.Object.extend('Polls');
-            const query = new Parse.Query(Polls);
-            const results = await query.find();
+            try {
+                const Polls = Parse.Object.extend('Polls');
+                const query = new Parse.Query(Polls);
+                const results = await query.find();
 
-            if (results.length > 0) {
-                const poll = results[0];
-                let votes = poll.get('votes');
-                if (!Array.isArray(votes) && typeof votes === 'object') {
-                    votes = Object.keys(votes).map(key => votes[key]);
-                    poll.set('votes', votes);
-                    await poll.save(); 
+                if (results.length > 0) {
+                    const poll = results[0];
+                    let votes = poll.get('votes');
+                    if (!Array.isArray(votes) && typeof votes === 'object') {
+                        votes = Object.keys(votes).map(key => votes[key]);
+                        poll.set('votes', votes);
+                        await poll.save(); 
+                    }
+
+                    setData(votes);
                 }
-
-                setData(votes);
+            } catch (error) {
+                console.error('Error fetching and migrating data:', error);
             }
         };
 
@@ -36,6 +41,15 @@ export default function Bar() {
             serverURL: env.LIVE_SERVER_URL,
             javascriptKey: env.PARSE_JAVASCRIPT_KEY,
         });
+
+        client.on('open', () => {
+            console.log('LiveQuery Client connected');
+        });
+
+        client.on('error', (error) => {
+            console.error('LiveQuery Client error:', error);
+        });
+
         client.open();
 
         const Polls = Parse.Object.extend('Polls');
@@ -43,53 +57,67 @@ export default function Bar() {
         const subscription = client.subscribe(query);
 
         subscription.on('update', (poll) => {
-            let votes = poll.get('votes');
-            if (!Array.isArray(votes) && typeof votes === 'object') {
-                votes = Object.keys(votes).map(key => votes[key]);
-            }
+            try {
+                let votes = poll.get('votes');
+                if (!Array.isArray(votes) && typeof votes === 'object') {
+                    votes = Object.keys(votes).map(key => votes[key]);
+                }
 
-            setData(votes);
+                setData(votes);
+            } catch (error) {
+                console.error('Error handling update event:', error);
+            }
         });
 
-});
+
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, []);
 
     const updateVote = async (id) => {
-        const Polls = Parse.Object.extend('Polls');
-        const query = new Parse.Query(Polls);
-        const poll = await query.first();
+        try {
+            const Polls = Parse.Object.extend('Polls');
+            const query = new Parse.Query(Polls);
+            const poll = await query.first();
 
-        if (poll) {
-            let votes = poll.get('votes');
+            if (poll) {
+                let votes = poll.get('votes');
 
-            if (Array.isArray(votes)) {
-                votes[id] += 1;
-                poll.set('votes', votes);
-                await poll.save();
-            } else {
-                console.error('Votes field is not an array.');
+                if (Array.isArray(votes)) {
+                    votes[id] += 1;
+                    poll.set('votes', votes);
+                    await poll.save();
+                } else {
+                    console.error('Votes field is not an array.');
+                }
             }
+        } catch (error) {
+            console.error('Error updating vote:', error);
         }
     };
 
     const clearVotes = async () => {
-        const Polls = Parse.Object.extend('Polls');
-        const query = new Parse.Query(Polls);
-        const poll = await query.first();
+        try {
+            const Polls = Parse.Object.extend('Polls');
+            const query = new Parse.Query(Polls);
+            const poll = await query.first();
 
-        if (poll) {
-            const numOptions = 5;
-            const resetVotes = Array(numOptions).fill(0);
-            poll.set('votes', resetVotes);
-            await poll.save();
-            setData(resetVotes);
+            if (poll) {
+                const numOptions = 5;
+                const resetVotes = Array(numOptions).fill(0);
+                poll.set('votes', resetVotes);
+                await poll.save();
+                setData(resetVotes);
+            }
+        } catch (error) {
+            console.error('Error clearing votes:', error);
         }
     };
 
     return (
         <>
-            <h4>Most Popular EzEats Restaurants
-                
-            </h4>
+            <h4>Most Popular EzEats Restaurants</h4>
             <div className='bar'>
                 <BarChart
                     width={800}
@@ -123,7 +151,7 @@ export default function Bar() {
                 <button className='myButton' onClick={() => updateVote(4)}>
                     Sudfa
                 </button>
-                </div>
+            </div>
             <div className='btn'>
                 <button className='myButton' onClick={clearVotes}>
                     Clear Votes
